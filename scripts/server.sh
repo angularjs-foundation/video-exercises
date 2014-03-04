c0fc70dca2e2cc7f62460053133132ae5d2ecc28
Matias Niemelä <matias@yearofmoo.com>
2014-03-03 20:15:30 -0500

    Code organized into separate directories

---
+++ scripts/server.sh
@@ -0,0 +1,2 @@
+open "http://localhost:8088/"
+node ./node_modules/http-server/bin/http-server -p 8088 .
--- server.sh
+++
@@ -1,2 +0,0 @@
-open "http://localhost:8088/"
-node ./node_modules/http-server/bin/http-server -p 8088 ./code
